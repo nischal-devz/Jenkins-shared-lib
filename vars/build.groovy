5ebd538802d8d9605aef31902d983c30aa798d01
Nischal Karmacharya <58777852+nischal-devz@users.noreply.github.com>
2026-05-12 03:16:22 +0545

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
 def call(String image_name, String image_tag, String dockerHub_User){
-  sh "docker build -t ${dockerHub_User}/${image_name}:{image_tag} ."
+  sh "docker build -t ${dockerHub_User}/${image_name}:${image_tag} ."
   echo "Image built sucessfully"
 }
